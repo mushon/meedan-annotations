ee8d89441ced2d78d87fd3a971436bd66f430445
mushon <mushon@shual.com>
2012-06-27 14:48:45 +0300

    started working on the sharing interface (I love Bootstrap!)

---
+++ prototype/js/bookmarklet.js
@@ -0,0 +1,23 @@
+////////////////
+// ADD SCREEN //
+////////////////
+
+
+
+//#btn-discuss
+$('#btn-discuss').click(function() {
+	$('#add').toggle();
+	$('#discuss').toggle();
+});
+
+
+////////////////
+// ADD SCREEN //
+////////////////
+
+//tabs
+
+$('#myTab a').click(function (e) {
+	e.preventDefault();
+	$(this).tab('show');
+});
