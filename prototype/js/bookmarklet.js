112c3b57115488f98d5532e19205c7a1a18571e6
mushon <mushon@shual.com>
2012-07-02 18:19:55 +0300

    started playing with CSS transitions (how fun…)

--- prototype/js/bookmarklet.js
+++ prototype/js/bookmarklet.js
@@ -18,3 +18,18 @@ $('#myTab a').click(function (e) {
 	e.preventDefault();
 	$(this).tab('show');
 });
+
+
+////////////////
+// SLIDE      //
+////////////////
+
+//onstage
+$(document).ready(function() {
+  $('#modal').addClass('onstage');
+});
+
+//slideout
+$('button.out').click(function (e) {
+	$('#modal').addClass('slideout');
+});
